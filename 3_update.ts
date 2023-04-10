05b696e9ff7a15b83b8921febd139604d519658a
tkx.cool <lizeyan1998@gmail.com>
2023-04-10 15:19:06 +0800

    feat: 添加更新操作

---
+++ 3_update.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // 删除
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- script.ts
+++ script.ts
@@ -4,6 +4,56 @@ const prisma = new PrismaClient();
 
 async function main() {
   // 更新
+  // 更新单个记录
+  // const user = await prisma.user.update({
+  //   where: {
+  //     email: "wangyi@qq.com",
+  //   },
+  //   data: {
+  //     name: "王懿儿",
+  //   },
+  // });
+  // console.log({ user });
+  // 更新多个记录
+  // const users = await prisma.user.updateMany({
+  //   where: {
+  //     email: {
+  //       contains: "gmail.com",
+  //     },
+  //   },
+  //   data: {
+  //     role: "ADMIN",
+  //   },
+  // });
+  // console.log({ users });
+  // 更新 或 创建记录
+  // const user = await prisma.user.upsert({
+  //   where: {
+  //     email: "viola@prisma.io",
+  //   },
+  //   update: {
+  //     name: "viola",
+  //   },
+  //   create: {
+  //     email: "viola@prisma.io",
+  //     name: "viola",
+  //   },
+  // });
+  // console.log({ user });
+  // 更新数字字段
+  const post = await prisma.post.updateMany({
+    where: {
+      published: true,
+    },
+    data: {
+      views: {
+        increment: 1,
+      },
+      likes: {
+        increment: 1,
+      },
+    },
+  });
 }
 
 main()
