f73e9b2cdb780e3de912c83a40053c66e212f40c
tkx.cool <lizeyan1998@gmail.com>
2023-04-11 11:03:20 +0800

    feat: 添加删除操作

--- script.ts
+++ script.ts
@@ -3,57 +3,74 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // 更新
-  // 更新单个记录
-  // const user = await prisma.user.update({
+  // 做删除操作前先生成需要操作的数据
+  // const users = await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: "test1",
+  //       email: "test1@qq.com",
+  //     },
+  //     {
+  //       name: "test2",
+  //       email: "test2@qq.com",
+  //     },
+  //     {
+  //       name: "test3",
+  //       email: "test3@qq.com",
+  //     },
+  //   ],
+  // });
+  // const user = await prisma.user.findUnique({
   //   where: {
-  //     email: "wangyi@qq.com",
+  //     email: "test3@qq.com",
   //   },
+  // });
+  // const post = await prisma.post.create({
   //   data: {
-  //     name: "王懿儿",
+  //     title: "test",
+  //     content: "test",
+  //     author_id: user?.id,
+  //   },
+  // });
+  // console.log(post);
+  // 删除
+  // 删除单条记录
+  // const user = await prisma.user.delete({
+  //   where: {
+  //     email: "test1@qq.com",
   //   },
   // });
   // console.log({ user });
-  // 更新多个记录
-  // const users = await prisma.user.updateMany({
+  // 删除多条记录
+  // const users = await prisma.user.deleteMany({
   //   where: {
   //     email: {
-  //       contains: "gmail.com",
+  //       contains: "test",
   //     },
-  //   },
-  //   data: {
-  //     role: "ADMIN",
   //   },
   // });
   // console.log({ users });
-  // 更新 或 创建记录
-  // const user = await prisma.user.upsert({
+  // 删除所有记录
+  // const users = await prisma.user.deleteMany({});
+  // 级联删除
+  // const deletePosts = prisma.post.deleteMany({
   //   where: {
-  //     email: "viola@prisma.io",
-  //   },
-  //   update: {
-  //     name: "viola",
-  //   },
-  //   create: {
-  //     email: "viola@prisma.io",
-  //     name: "viola",
+  //     author_id: 22,
   //   },
   // });
-  // console.log({ user });
-  // 更新数字字段
-  const post = await prisma.post.updateMany({
-    where: {
-      published: true,
-    },
-    data: {
-      views: {
-        increment: 1,
-      },
-      likes: {
-        increment: 1,
-      },
-    },
-  });
+  // const deleteUsers = prisma.user.delete({
+  //   where: {
+  //     id: 22,
+  //   },
+  // });
+  // const result = await prisma.$transaction([deletePosts, deleteUsers]);
+  // console.log(result);
+  // 从所有表中删除所有记录
+  // 事务同步进行，因此deleteUsers必须最后执行
+  // const deletePosts = prisma.post.deleteMany({});
+  // const deleteProfiles = prisma.profile.deleteMany({});
+  // const deleteUsers = prisma.user.deleteMany({});
+  // await prisma.$transaction([deletePosts, deleteProfiles, deleteUsers]);
 }
 
 main()
